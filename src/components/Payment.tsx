e99b70e62cf1d918f521d06c0572d55e15e190c9
Frank Condezo <frankcondezo@gmail.com>
2022-11-18 20:28:29 -0500

    add filters to payments

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -1,17 +1,45 @@
 import React from "react";
-import { Box, Checkbox } from "@chakra-ui/react";
+import { Box, Checkbox, Input, Stack } from "@chakra-ui/react";
 import { useLocalStorage } from "./hooks";
+import { Radio, RadioGroup } from "@chakra-ui/react";
 
 function Payment({ players }: { players: string[] }) {
   const [payments, setPayments] = useLocalStorage<Record<string, boolean>>(
     "payments",
     {}
   );
+  const [search, setSearch] = React.useState("");
+  const [filterPay, setFilterPay] = React.useState("all");
+
+  function filteredPlayers() {
+    return players
+      .filter((player) => player.toLowerCase().includes(search.toLowerCase()))
+      .filter((player) => {
+        if (filterPay === "all") return true;
+
+        if (filterPay === "pay") return payments[player];
+
+        return !payments[player];
+      });
+  }
 
   return (
     <Box>
-      <Box mb="4">
-        {players.map((player, index) => (
+      <Input
+        placeholder="Buscar"
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+        mb="2"
+      />
+      <RadioGroup onChange={setFilterPay} value={filterPay}>
+        <Stack direction="row">
+          <Radio value={"all"}>All</Radio>
+          <Radio value={"no-pay"}>No pago</Radio>
+          <Radio value={"pay"}>Pago</Radio>
+        </Stack>
+      </RadioGroup>
+      <Box my="4">
+        {filteredPlayers().map((player, index) => (
           <Box key={index} mr="4">
             <Checkbox
               isChecked={payments[player]}
